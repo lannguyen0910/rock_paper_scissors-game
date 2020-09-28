8316e861a503c6b28239a6481e3f71d8496a6250
lannguyen0910 <47696901+coolboyteo2k@users.noreply.github.com>
2020-09-27 22:18:37 -0700

    Fix some bugs - model still not good

--- test.py
+++ test.py
@@ -4,27 +4,23 @@ import torch.nn.functional as F
 from torchvision import transforms, datasets, models
 from torch.autograd import Variable
 import numpy as np
-import pandas as pd
 import os
 import random
-
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
+import cv2
+#device = torch.device('cpu')
 resnet = models.resnet50(pretrained=True)
-resnet = resnet.to(device)
 
 for params in resnet.parameters():
     params.requires_grad = False
 
 in_features = resnet.fc.in_features
-resnet.fc = nn.Linear(in_features = in_features, out_features=3).to(device)
-
-resnet.load_state_dict(torch.load('model1.pt'))
+resnet.fc = nn.Linear(in_features=in_features, out_features=3)
+resnet.load_state_dict(torch.load('model2.pt', map_location=torch.device('cpu')))
 
 
 IMG_SIZE = 128
-options = ['rock', 'paper', 'scissor']
-winRule = {'rock': 'scissor', 'scissor': 'paper', 'paper': 'rock'}
+options = ['rock', 'paper', 'scissors']
+winRule = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
 rounds = 0
 botScore = 0
 playerScore = 0
@@ -35,7 +31,7 @@ def prepImg(path):
 
 
 def updateScore(play, bplay, p, b):
-    winRule = {'rock': 'scissor', 'scissor': 'paper', 'paper': 'rock'}
+    winRule = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
     if play == bplay:
         return p, b
     elif bplay == winRule[play]:
@@ -43,16 +39,26 @@ def updateScore(play, bplay, p, b):
     else:
         return p, b+1
 
+def inv_normalize(img):
+    mean = torch.Tensor([0.4914, 0.4822, 0.4465]).unsqueeze(-1)
+    std= torch.Tensor([0.2023, 0.1994, 0.2010]).unsqueeze(-1)
+    img = (img.view(3, -1) * std + mean).view(img.shape)
+    img = img.clamp(0, 1)
+    return img
 
 def predict_images(model, images):
     model.eval()
 
     with torch.no_grad():
-        test = Variable(images).view(-1, 3, IMG_SIZE, IMG_SIZE)
-        test = test.to(device)
+     
+        image = torch.from_numpy(images)
+        image = inv_normalize(image)
+        test = Variable(image).view(-1, 3, IMG_SIZE, IMG_SIZE)
+        #test = test.to(device)
 
         outputs = model(test)
         preds = F.softmax(outputs.data, dim=1)
+        print('Preds: ', preds)
 
     return preds.cpu().numpy()[0]
 
@@ -62,16 +68,16 @@ ret, frame = cap.read()
 
 NUM_ROUNDS = 3
 bplay = ""
-
+labels_dict = {0: 'paper', 1: 'rock', 2: 'scissors'}
 
 while True:
     ret, frame = cap.read()
-    if ret:
-        assert isinstance(frame, type(None)), 'frame not found'
+#     if ret:
+#         assert not isinstance(frame, type(None)), 'frame not found'
     # print(ret)
     frame = cv2.putText(frame, "Press Space to start", (160, 200),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
-    cv2.imshow('Rock Paper Scissors',frame)
+    cv2.imshow('Rock paper scissors', frame)
     if cv2.waitKey(1) & 0xff == ord(' '):
         break
 
@@ -88,8 +94,9 @@ for rounds in range(NUM_ROUNDS):
         # Prediction
         elif i/20 < 3.5:
             pred = np.argmax(predict_images(
-                resnet, prepImg(frame[0:200, 0:200])))
+                resnet, prepImg(frame[100: 300, 150: 350])))
             pred = labels_dict[pred]
+
         # Get Bots Move
         elif i/20 == 3.5:
             bplay = random.choice(options)
@@ -125,7 +132,7 @@ while True:
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
     frame = cv2.putText(frame, "Press q to quit", (190, 200),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
-    frame = cv2.putText(frame, "Player : {}      AI : {}".format(
+    frame = cv2.putText(frame, "Player : {}      Bot : {}".format(
         playerScore, botScore), (120, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (250, 250, 0), 2, cv2.LINE_AA)
     cv2.imshow('Rock Paper Scissor', frame)
     if cv2.waitKey(1) & 0xff == ord('q'):
